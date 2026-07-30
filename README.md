16aa771bd3a9a0974aa4c6799a2daa97271e2bb4
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2026-07-30 14:28:41 -0500

    add Globus OAuth in a dedicated branch

--- README.md
+++ README.md
@@ -12,6 +12,6 @@ This is a mimimal demonstration of how to consume the OAuth2 Authorization Code 
 
 3. `uv sync`
 
-4. `uv run --env-file=dev.env fastapi dev main.py`
+4. `uv run --env-file=dev.env fastapi dev main_globus.py`
 
 5. Visit the running demo at http://127.0.0.1:8000/
--- settings.py
+++ settings.py
@@ -6,3 +6,7 @@ TAPIS_OAUTH_CALLBACK_URL = os.environ.get(
 )
 TAPIS_OAUTH_CLIENT_ID = os.environ.get("TAPIS_OAUTH_CLIENT_ID", "DEV.TMS-PROTO")
 TAPIS_OAUTH_CLIENT_KEY = os.environ.get("TAPIS_OAUTH_CLIENT_KEY", "")
+
+GLOBUS_OAUTH_URL= os.environ.get("GLOBUS_OAUTH_URL", "https://auth.globus.org")
+GLOBUS_OAUTH_CLIENT_ID = os.environ.get("GLOBUS_OAUTH_CLIENT_ID", "")
+GLOBUS_OAUTH_CLIENT_SECRET = os.environ.get("GLOBUS_OAUTH_CLIENT_SECRET", "")
